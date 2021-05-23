caa510ef708bc46e76ed8b974d3563edc2fb33b8
Omkar Gurav <64888669+OmkarGurav6@users.noreply.github.com>
2021-05-23 20:11:27 +0530

    Update airquality.R

--- SEM 6/DSBDA/DSBDA Assignments/Group B/Assignment 2/airquality.R
+++ SEM 6/DSBDA/DSBDA Assignments/Group B/Assignment 2/airquality.R
@@ -21,9 +21,9 @@
    
    # Solution 2 to remove NA values : Replace NA value with mean value 
    
-   my_airquality_data$Ozone[is.na(my_airquality_data$Ozone)] <- mean(my_airquality_data$Ozone, na.rm = TRUE)
+   my_airquality_data$Ozone[is.na(my_airquality_data$Ozone)] <- as.integer(mean(my_airquality_data$Ozone, na.rm = TRUE))
    
-   my_airquality_data$Solar.R[is.na(my_airquality_data$Solar.R)] <- mean(my_airquality_data$Solar.R, na.rm = TRUE)
+   my_airquality_data$Solar.R[is.na(my_airquality_data$Solar.R)] <- as.integer(mean(my_airquality_data$Solar.R, na.rm = TRUE))
    
    # Checking if there are any NA values
    sum(is.na(my_airquality_data))
@@ -31,9 +31,45 @@
    
   # Data integration
   
+   my_airquality_data_subset_1 <- my_airquality_data[1:10, c(2,3)]
+
+   my_airquality_data_subset_2 <- my_airquality_data[1:10, c(4,5)]   
    
+   cbind(my_airquality_data_subset_1, my_airquality_data_subset_2)
+
    
+  # Data transformation
+
+   copy$Month <- month.abb[copy$Month]
+
+  
+  # Data model building (regression model for prediction of Ozone value)   
    
+   # Normalizing the dataset so that no particular attribute has more impact on clustering algorithm than others.
+   normalize <- function(x){
+      
+      return((x-min(x))/(max(x)-min(x)))
    
+   }
    
+   # replace contents of dataset with normalized values
+   my_airquality_data_normalized <- normalize(my_airquality_data) 
+   str(my_airquality_data_normalized)    
+   
+   # Setting predictor attribute
+   solar_R <- my_airquality_data_normalized[, "Solar.R"]
+   
+   # Setting target attribute
+   ozone <- my_airquality_data_normalized[, "Ozone"]
+   
+   # Fitting linear model
+   model_ozone_solar_R <- lm(ozone~solar_R)
+   model_ozone_solar_R        # Gives values of y-intercept and slope         
+   
+   plot(ozone~solar_R)
+   
+   # Prediction of 'Ozone' when 'Solar.R'= 20
+   p1 <- predict(model_ozone_solar_R, data.frame("solar_R" = 20))
+   p1   
+      
    
